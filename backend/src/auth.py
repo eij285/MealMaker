462528425714a56941b3dabc23c6dba661b34e26
Elijah Lee <eij285@gmail.com>
2022-10-08 16:23:44 +1100

    documented javadocs for all auth functions, extra functions needed for resetting password

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -5,17 +5,23 @@ def auth_register(username, email, password):
     """Registers a new user
 
     Registers a user by adding them to the database if they were not already
-    registered and information provided is valid. Should return a success/fail
-    status message, as well as a JWT token so they can immediately start
-    browsing.
+    registered and email and password provided is valid. If so, their login 
+    details should be saved (with password hashed using bcrypt) for future
+    logins. It should also return a JWT token so that the user can immediately
+    begin browsing the product.
+
+    https://zetcode.com/python/bcrypt/
 
     Args:
-        username (str): 
-        email (str):
-        password (str):
+        username    (String): username to be used in future logins
+        email       (String): email to be used in future logins
+        password    (String): password to be used in future logins
 
     Returns:
-        ...
+        Status 201
+            token:  String
+        Status 400
+            errors: [String]
 
     """
     
@@ -35,7 +41,7 @@ def auth_register(username, email, password):
     conn.close()
     pass
 
-def auth_login():
+def auth_login(login, password):
     """Logins a user
     
     Logins a user and assigns them a new JWT token for the current session.
@@ -43,37 +49,53 @@ def auth_login():
     user in the database.
 
     Args:
-        ...
+        login       (String): username or email being used to login
+        password    (String): password being used to login
 
     Returns:
-        ...
+        Status 200
+            token:  String
+        Status 400
+            errors: [String]
     
     """
     pass
 
-def auth_update_pw():
+def auth_update_pw(token, password):
     """Updates an authenticated user's password
     
-    ...
+    Updates a user's password after confirming they are a valid authenticated
+    user. New password must meet the minimum requirements.
     
     Args:
-        ...
+        token       (String): token of authenticated user
+        password    (String): new password to replace old password of
+                              authenticated user
+
         
     Returns:
-        ...
+        Status 200
+        Status 400
+        Status 401
     
     """
 
-def auth_reset_pw():
-    """Resets an authenticated user's password
+def auth_reset_pw(email):
+    """Resets a user's password
     
-    ...
+    Allows a user to reset their password if they forgot it. This is done by
+    sending an email containing a secure link which allows them to reset their
+    password. Password must meet the minimum requirements.
+
+    url query string containing email and reset code
+    reset code 32 bit hashed
     
     Args:
-        ...
+        email       (String): email that the reset link will be sent to
         
     Returns:
-        ...
+        Status 200
+        Status 400
         
     """
     pass
