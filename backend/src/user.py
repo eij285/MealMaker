7c835c4483cb1050c2a008c39eef97379a4f3560
Owen Tang <owentang213@gmail.com>
2022-10-09 12:16:47 +1100

    created stubs/skeletons for user functions sprint 1

---
+++ backend/src/user.py
@@ -0,0 +1,109 @@
+import psycopg2
+
+def user_units():
+    """Changes users prefered form of measurement
+
+    Checks a registered users prefered form of measurement (metric or imperial). User can then
+    Toggle the measurement method into the one that is the opposite of what was already selected
+    The default measurement would be metric.
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_efficiency():
+    """Changes users cooking speed
+
+    Checks a registered users cooking speed. User can then
+    change their cooking speed so that recipe display suggested time will be adjusted
+    default would be beginner but can choose to change to intermediate or expert
+
+    Args:
+
+    Returns:
+
+    """
+
+
+def user_update_name():
+    """Changes users name
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_surname():
+    """Changes users surname
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_display_name():
+    """Changes users display name
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_email():
+    """Changes users email
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_about_me():
+    """Changes users about me
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_country():
+    """Changes users country
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_visibility():
+    """Changes users visibilty
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_profile_picture():
+    """Changes users profile picture
+
+    Args:
+
+    Returns:
+
+    """
+
+def user_update_password():
+    """Changes users password
+
+    Args:
+
+    Returns:
+
+    """
