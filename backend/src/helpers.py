01d590f1106348acd50e205864d7e2a030b7e266
Goce (George) Pacarski <z5260639@unsw.edu.au>
2022-10-09 15:01:02 +1100

    added sql script execution to reset database

---
+++ backend/src/helpers.py
@@ -0,0 +1,46 @@
+import psycopg2
+from config import DB_CONN_STRING
+
+def database_reset():
+    """Resets the database
+    
+    Drops all tables, stored procedures, triggers, etc and recreates entire
+    schema from an sql file
+
+    Returns:
+        True if database reset succeeded
+        False if database reset failed (requires manual intervention)
+    """
+    conn = None
+    cur = None
+    success = False
+    try:
+        conn = psycopg2.connect()
+        cur = conn.cursor()
+        cur.execute(open("schema.sql", "r").read())
+        conn.commit()
+        success = True
+    except psycopg2.Error as err:
+        print(f"DB error: {err}")
+    except OSError as err:
+        print(f"OS error: {err}")
+    except:
+        print("Unspecified error")
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+    return success
+
+def files_reset():
+    """Files reset
+    
+    Deletes any files created by the application to reset file content
+    back to initial state
+
+    Returns:
+        True if file deleted successfully
+        False if file deletion failed (requires manual intervention)
+    """
+    pass
