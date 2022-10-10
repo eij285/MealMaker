bb6d39658249d941460105cf95cea8739e7dcd10
jeffreySeto <z5259101@ad.unsw.edu.au>
2022-10-10 23:26:49 +1100

    added extra line for connection accounting for inconsistent dbname

--- backend/src/backend_helper.py
+++ backend/src/backend_helper.py
@@ -6,6 +6,7 @@ def connect():
     try:
         print('Conencting to the database...')
         connection = psycopg2.connect(database="meal_maker",host="localhost",user="postgres",password="000000",port="5432")
+        #connection =  psycopg2.connect(database="codechefs-db")
         cur = connection.cursor()
         print('PostgreSQL database version: ')
         cur.execute('SELECT version()')
