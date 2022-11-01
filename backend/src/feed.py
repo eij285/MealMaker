809a749fd9c92b036d02000993fd20a11984fa8e
Elijah Lee <eij285@gmail.com>
2022-11-02 08:36:55 +1100

    tried doing trending, some issues rewriting george's recipe_details to work with no token/u_id

--- backend/src/feed.py
+++ backend/src/feed.py
@@ -233,8 +233,117 @@ def feed_fetch_subscription(token):
     pass
 
 def feed_fetch_trending():
-    pass
+    
+    # Connect to database
+    try:
+        conn = psycopg2.connect(DB_CONN_STRING)
+        cur = conn.cursor()
+    except:
+        return {
+            'status_code': 500,
+            'error': 'Unable to connect to database'
+        }
+    
+    sql_query = "SELECT r.recipe_id, COUNT(l.recipe_id) \
+                 FROM recipes r \
+                 LEFT JOIN recipe_user_likes l \
+                 ON (r.recipe_id = l.recipe_id) \
+                 WHERE r.recipe_status = 'published' \
+                 GROUP BY r.recipe_id \
+                 ORDER BY COUNT(l.recipe_id) DESC;"
+    cur.execute(sql_query)
+
+    sql_result = cur.fetchall()
+
+    # If empty results, then return empty body
+    if not sql_result:
+        cur.close()
+        conn.close()
+
+        return {
+            'status_code': 200,
+            'body': {}
+        }
+
+    # For each recipe_id, add recipe details to body content
+    body_content = []
+    
+    for recipe in sql_result:
+        r_id = recipe[0]
+        user_id, units, efficiency = -1, 'Metric', 'Intermediate'
+        
+        sql_query = "SELECT u.id, u.display_name, u.base64_image, \
+                     r.recipe_name, r.recipe_description, r.recipe_photo, \
+                     r.recipe_status, r.recipe_method, r.created_on, \
+                     r.edited_on, r.preparation_hours, r.preparation_minutes, \
+                     r.servings, r.energy, r.protein, r.carbohydrates, r.fats \
+                     r.cuisine, r.breakfast, r.lunch, r.dinner, r.snack, \
+                     r.vegetarian, r.vegan, r.kosher, r.halal, r.dairy_free \
+                     r.gluten_free, r.nut_free, r.egg_free, r.shellfish_free \
+                     r.soy_free \
+                     FROM users u JOIN recipes r ON (u.id = r.owner_id) \
+                     WHERE r.recipe_id = %s"
+        cur.execute(sql_query, (r_id,))
+        
+        r_details = cur.fetchone()
+        
+        if not r_details:
+            return {
+                'status_code': 400,
+                'error': 'Database error: recipe has been removed unexpectedly'
+            }
+        
+        body_content.append({
+            'author_id': recipe[0],
+            'author_display_name': recipe[1],
+            'author_image': recipe[2],
+            'recipe_name': recipe[3],
+            'recipe_description': recipe[4],
+            'recipe_photo': recipe[5],
+            'recipe_status': recipe[6],
+            'recipe_method': recipe[7],
+            'created_on': str(recipe[8]),
+            'edited_on': str(recipe[9]),
+            'preparation_hours': recipe[10],
+            'preparation_minutes': recipe[11],
+            'servings': recipe[12],
+            'energy': recipe[13],
+            'protein': recipe[14],
+            'carbohydrates': recipe[15],
+            'fats': recipe[16],
+            'cuisine': recipe[17],
+            'breakfast': recipe[18],
+            'lunch': recipe[19],
+            'dinner': recipe[20],
+            'snack': recipe[21],
+            'vegetarian': recipe[22],
+            'vegan': recipe[23],
+            'kosher': recipe[24],
+            'halal': recipe[25],
+            'dairy_free': recipe[26],
+            'gluten_free': recipe[27],
+            'nut_free': recipe[28],
+            'egg_free': recipe[29],
+            'shellfish_free': recipe[30],
+            'soy_free': recipe[31],
+            'units': str(units),
+            'efficiency': str(efficiency),
+            'ingredients': ingredients_list,
+            'reviews': reviews,
+            'likes': likes,
+            'user_is_author': False
+        })
+        
+
+
+    return {
+        'status_code': 200,
+        'body': body_content
+    }
+
+
 
 if __name__ == "__main__":
     pprint(feed_fetch_discover("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1X2lkIjoxMiwiZXhwIjoxNjY3OTA2MDE4fQ.v9cREARALjt-Lj6AgyphTKRTCBJCQT393Ct1cozT9Ew"))
+    pprint(feed_fetch_trending())
 
