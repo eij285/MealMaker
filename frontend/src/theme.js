2a216b89a5a82d546687902f65b89bed488976ae
Lachlan Bailey <lachlan@fastmail.com>
2022-11-03 19:45:01 +1100

    Added theme and chef hat images which I forgot to add in previous commit.

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffcd02',
+        },
+        secondary: {
+            main: '#33319f',
+        },
+    },
+});
+
+export default theme
